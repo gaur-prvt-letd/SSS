d549814a088ecebc93ee4912d7dbefe89a353c5c
Surender Singh <surenderrajput9211@gmail.com>
2025-09-19 00:37:18 +0530

    updated code of the login page

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,25 +1,22 @@
-// src/components/common/Button.tsx
-import { Button as MuiButton } from "@mui/material";
+// src/components/common/CustomButton.tsx
+import { Button as MuiButton, type ButtonProps } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
-interface Props {
-  children: React.ReactNode;
-  onClick?: () => void;
-}
+type CustomButtonProps = ButtonProps;
 
-export const Button = ({ children, onClick }: Props) => {
+export const Button = (props: CustomButtonProps) => {
   const theme = useTheme();
+  const { sx, ...rest } = props;
 
   return (
     <MuiButton
-      onClick={onClick}
+      {...rest}
       sx={{
         backgroundColor: theme.palette.primary.main,
         color: theme.palette.primary.contrastText,
         "&:hover": { backgroundColor: theme.palette.primary.dark },
+        ...sx,
       }}
-    >
-      {children}
-    </MuiButton>
+    />
   );
 };
